13f920afc2bf1bb6e01486f82c54f7172ff5ae1d
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2021-03-02 07:22:48 +0330

    pascal triangle

---
+++ CombinatoricsProbability/pascal.py
@@ -0,0 +1,7 @@
+C = dict()  # C ( [ n , k ] )   i s   equal  to  n  choose  k
+for n in range(8):
+    C[n, 0] = 1
+    C[n, n] = 1
+    for k in range(1, n):
+        C[n, k] = C[n - 1, k - 1] + C[n - 1, k]
+print(C[7, 4])
